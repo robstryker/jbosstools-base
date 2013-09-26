6dc733bce60dacf4856bc6f591b139281a394230
Rob Stryker <rob@oxbeef.net>
2013-09-26 15:59:47 +0800

    Cleaning up patch for maintenance to be less 'busy'

--- runtime/plugins/org.jboss.tools.runtime.ui/src/org/jboss/tools/runtime/ui/internal/wizard/FinalizeRuntimeDownloadFragment.java
+++ runtime/plugins/org.jboss.tools.runtime.ui/src/org/jboss/tools/runtime/ui/internal/wizard/FinalizeRuntimeDownloadFragment.java
@@ -10,19 +10,33 @@
  ************************************************************************************/
 package org.jboss.tools.runtime.ui.internal.wizard;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.fieldassist.ControlDecoration;
 import org.eclipse.jface.fieldassist.FieldDecoration;
 import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -43,10 +57,13 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.progress.IProgressService;
+import org.jboss.tools.foundation.core.ecf.URLTransportUtility;
 import org.jboss.tools.runtime.core.model.DownloadRuntime;
+import org.jboss.tools.runtime.core.model.RuntimeDefinition;
+import org.jboss.tools.runtime.core.model.RuntimePath;
+import org.jboss.tools.runtime.core.util.RuntimeInitializerUtil;
 import org.jboss.tools.runtime.ui.Messages;
 import org.jboss.tools.runtime.ui.RuntimeUIActivator;
-import org.jboss.tools.runtime.ui.internal.wizard.util.DownloadRuntimeOperationUtility;
 import org.jboss.tools.runtime.ui.taskwizard.IWizardHandle;
 import org.jboss.tools.runtime.ui.taskwizard.WizardFragment;
 import org.jboss.tools.runtime.ui.wizard.DownloadRuntimesTaskWizard;
@@ -58,6 +75,7 @@ import org.jboss.tools.runtime.ui.wizard.DownloadRuntimesTaskWizard;
  */
 public class FinalizeRuntimeDownloadFragment extends WizardFragment {
 
+	private static final String SEPARATOR = "/"; //$NON-NLS-1$
 	private static final String FOLDER_IS_REQUIRED = Messages.DownloadRuntimesSecondPage_This_folder_is_required;
 	private static final String FOLDER_IS_NOT_WRITABLE = Messages.DownloadRuntimesSecondPage_This_folder_does_not_exist;
 	private static final String DELETE_ON_EXIT = "deleteOnExit"; //$NON-NLS-1$
@@ -87,6 +105,34 @@ public class FinalizeRuntimeDownloadFragment extends WizardFragment {
 	private Shell shell;
 	
 	private IWizardHandle handle;
+	
+	IOverwrite overwriteQuery = new IOverwrite() {
+		public int overwrite(File file) {
+			final String msg = NLS.bind(Messages.DownloadRuntimesSecondPage_The_file_already_exists, file.getAbsolutePath()); 
+			final String[] options = { IDialogConstants.YES_LABEL,
+					IDialogConstants.YES_TO_ALL_LABEL,
+					IDialogConstants.NO_LABEL,
+					IDialogConstants.NO_TO_ALL_LABEL,
+					IDialogConstants.CANCEL_LABEL };
+			final int[] retVal = new int[1];
+			Display.getDefault().syncExec(new Runnable() {
+				public void run() {
+					Shell shell = PlatformUI.getWorkbench().getModalDialogShellProvider().getShell();
+					MessageDialog dialog = new MessageDialog(shell, Messages.DownloadRuntimesSecondPage_Question,
+							null, msg, MessageDialog.QUESTION, options, 0) {
+						protected int getShellStyle() {
+							return super.getShellStyle() | SWT.SHEET;
+						}
+					};
+					dialog.open();
+					retVal[0] = dialog.getReturnCode();
+				}
+			});
+			return retVal[0];
+		}
+	};
+
+	
 	
 	public FinalizeRuntimeDownloadFragment() {
 		dialogSettings = RuntimeUIActivator.getDefault().getDialogSettings();
@@ -557,7 +603,7 @@ public class FinalizeRuntimeDownloadFragment extends WizardFragment {
 			@Override
 			public IStatus run(IProgressMonitor monitor) {
 				monitor.beginTask("Download '" + downloadRuntime.getName() + "' ...", 100);//$NON-NLS-1$ //$NON-NLS-2$
-				IStatus ret = new DownloadRuntimeOperationUtility().downloadAndInstall(selectedDirectory, destinationDirectory, 
+				IStatus ret = downloadAndInstall(selectedDirectory, destinationDirectory, 
 						getDownloadUrl(), deleteOnExit, monitor);
 				if( monitor.isCanceled()) 
 					return Status.CANCEL_STATUS;
@@ -575,7 +621,188 @@ public class FinalizeRuntimeDownloadFragment extends WizardFragment {
 		return true;
 	}
 	
-	
+
+	private IStatus downloadAndInstall(String selectedDirectory, String destinationDirectory, 
+			String urlString, boolean deleteOnExit, IProgressMonitor monitor) {
+		FileInputStream in = null;
+		OutputStream out = null;
+		File file = null;
+		try {
+			URL url = new URL(urlString);
+			String name = url.getPath();
+			int slashIdx = name.lastIndexOf('/');
+			if (slashIdx >= 0)
+				name = name.substring(slashIdx + 1);
+			
+			File destination = new File(destinationDirectory);
+			destination.mkdirs();
+			file = new File (destination, name);
+			int i = 1;
+			boolean download = true;
+			long urlModified = 0;
+			if (deleteOnExit) {
+				while (file.exists()) {
+					file = new File(destination, name + "(" + i++ + ")"); //$NON-NLS-1$ //$NON-NLS-2$
+				}
+			} else {
+				long cacheModified = file.lastModified();
+				try {
+					urlModified = new URLTransportUtility()
+							.getLastModified(url);
+					download = cacheModified <= 0 || cacheModified != urlModified;
+				} catch (CoreException e) {
+					// ignore
+				}
+			}
+			if (deleteOnExit) {
+				file.deleteOnExit();
+			}
+			IStatus result = null;
+			if (download) {
+				out = new BufferedOutputStream(new FileOutputStream(file));
+				result = new URLTransportUtility().download(
+						file.getName(), url.toExternalForm(), out, new SubProgressMonitor(monitor, 99));
+				out.flush();
+				out.close();
+				if (urlModified > 0) {
+					file.setLastModified(urlModified);
+				}
+			}
+			if (monitor.isCanceled()) {
+				file.deleteOnExit();
+				file.delete();
+				return Status.CANCEL_STATUS;
+			}
+			File directory = new File(selectedDirectory);
+			directory.mkdirs();
+			if (!directory.isDirectory()) {
+				final String message = "The '" + directory + "' is not a directory.";//$NON-NLS-1$ //$NON-NLS-2$
+				if (result != null) {
+					RuntimeUIActivator.getDefault().getLog().log(result);
+				} else {
+					RuntimeUIActivator.log(message);
+				}
+				Display.getDefault().syncExec(new Runnable() {
+
+					@Override
+					public void run() {
+						MessageDialog.openError(getActiveShell(), "Error", message);//$NON-NLS-1$
+					}
+					
+				});
+				file.deleteOnExit();
+				file.delete();
+				return Status.CANCEL_STATUS;
+			}
+			String root = getRoot(file, monitor);
+			if (monitor.isCanceled()) {
+				return Status.CANCEL_STATUS;
+			}
+
+			final IStatus status = unzip(file, directory, monitor, overwriteQuery);
+			if (monitor.isCanceled()) {
+				return Status.CANCEL_STATUS;
+			}
+			if (!status.isOK()) {
+				Display.getDefault().syncExec(new Runnable() {
+
+					@Override
+					public void run() {
+						MessageDialog.openError(getActiveShell(), "Error", status.getMessage());//$NON-NLS-1$
+					}
+					
+				});
+			}
+			
+			if (root != null) {
+				File rootFile = new File(selectedDirectory, root);
+				if (rootFile != null && rootFile.exists()) {
+					selectedDirectory = rootFile.getAbsolutePath();
+				}
+			}
+			createRuntimes(selectedDirectory, monitor);
+		} catch (IOException e) {
+			RuntimeUIActivator.log(e);
+			if (file != null && file.exists()) {
+				file.deleteOnExit();
+				file.delete();
+			}
+			final String message = e.getMessage();
+			Display.getDefault().syncExec(new Runnable() {
+
+				@Override
+				public void run() {
+					MessageDialog.openError(getActiveShell(), "Error", message);//$NON-NLS-1$
+				}
+				
+			});
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+		return Status.OK_STATUS;
+	}
+
+	private String getRoot(File file, IProgressMonitor monitor) {
+		ZipFile zipFile = null;
+		String root = null;
+		try {
+			zipFile = new ZipFile(file);
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+			while (entries.hasMoreElements()) {
+				if (monitor.isCanceled()) {
+					return null;
+				}
+				ZipEntry entry = (ZipEntry) entries.nextElement();
+				String entryName = entry.getName();
+				if (entryName == null || entryName.isEmpty() 
+						|| entryName.startsWith(SEPARATOR) || entryName.indexOf(SEPARATOR) == -1) {
+					return null;
+				}
+				String directory = entryName.substring(0, entryName.indexOf(SEPARATOR));
+				if (root == null) {
+					root = directory;
+					continue;
+				}
+				if (!directory.equals(root)) {
+					return null;
+				}
+			}
+		} catch (IOException e) {
+			RuntimeUIActivator.log(e);
+			final String message = e.getMessage();
+			Display.getDefault().syncExec(new Runnable() {
+
+				@Override
+				public void run() {
+					MessageDialog.openError(getActiveShell(), "Error", message);//$NON-NLS-1$
+				}
+				
+			});
+			return null;
+		} finally {
+			if (zipFile != null) {
+				try {
+					zipFile.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+		return root;
+	}
 
 	private Shell getActiveShell() {
 		Display display = Display.getDefault();
@@ -587,6 +814,35 @@ public class FinalizeRuntimeDownloadFragment extends WizardFragment {
 	}
 
 
+	private static void createRuntimes(String directory,
+			IProgressMonitor monitor) {
+		monitor.subTask("Creating runtime from location " + directory); //$NON-NLS-1$
+		final RuntimePath runtimePath = new RuntimePath(directory);
+		List<RuntimeDefinition> runtimeDefinitions = RuntimeInitializerUtil.createRuntimeDefinitions(runtimePath, monitor);
+		RuntimeUIActivator.getDefault().getModel().addRuntimePath(runtimePath);
+		if (runtimeDefinitions.size() == 0) {
+			openRuntimeNotFoundMessage();
+		} else if (runtimeDefinitions.size() > 1) {
+			Display.getDefault().asyncExec(new Runnable() {
+				public void run() {
+					RuntimeUIActivator.launchSearchRuntimePathDialog(
+							Display.getDefault().getActiveShell(),
+							RuntimeUIActivator.getDefault().getModel().getRuntimePaths(), false, 7);
+				}
+			});
+		} else /* size == 1 */{
+			RuntimeInitializerUtil.initializeRuntimes(runtimeDefinitions);
+		}
+		monitor.done();
+	}
+	private static void openRuntimeNotFoundMessage() {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				MessageDialog.openError(Display.getDefault()
+						.getActiveShell(), "Error", Messages.DownloadRuntimesSecondPage_No_runtime_server_found);//$NON-NLS-1$
+			}
+		});
+	}
 	private static void openErrorMessage(final String msg, boolean log) {
 		openErrorMessage( "Error", msg, log); //$NON-NLS-1$
 	}
@@ -610,5 +866,91 @@ public class FinalizeRuntimeDownloadFragment extends WizardFragment {
 		});		
 	}
 
+
+	private static IStatus unzip(File file, File destination,
+			IProgressMonitor monitor, IOverwrite overwriteQuery) {
+		ZipFile zipFile = null;
+		int overwrite = IOverwrite.NO;
+		destination.mkdirs();
+		try {
+			zipFile = new ZipFile(file);
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+			monitor.done();
+			monitor.beginTask(Messages.DownloadRuntimesSecondPage_Extracting, zipFile.size());
+			while (entries.hasMoreElements()) {
+				monitor.worked(1);
+				if (monitor.isCanceled() || overwrite == IOverwrite.CANCEL) {
+					return Status.CANCEL_STATUS;
+				}
+				ZipEntry entry = (ZipEntry) entries.nextElement();
+				File entryFile = new File(destination, entry.getName());
+				monitor.subTask(entry.getName());
+				if (overwrite != IOverwrite.ALL && overwrite != IOverwrite.NO_ALL && entryFile.exists()) {
+					overwrite = overwriteQuery.overwrite(entryFile);
+					switch (overwrite) {
+					case IOverwrite.CANCEL:
+						return Status.CANCEL_STATUS;
+					default:
+						break;
+					}
+				}
+				if (!entryFile.exists() || overwrite == IOverwrite.YES || overwrite == IOverwrite.ALL) {
+					createEntry(monitor, zipFile, entry, entryFile);
+				}
+			}
+		} catch (IOException e) {
+			return new Status(IStatus.ERROR, RuntimeUIActivator.PLUGIN_ID, e.getLocalizedMessage(), e);
+		} finally {
+			if (zipFile != null) {
+				try {
+					zipFile.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+			monitor.done();
+		}
+		return Status.OK_STATUS;
+	}
+
+	private static void createEntry(IProgressMonitor monitor, ZipFile zipFile,
+			ZipEntry entry, File entryFile) throws IOException,
+			FileNotFoundException {
+		monitor.setTaskName(Messages.DownloadRuntimesSecondPage_Extracting + entry.getName());
+		if (entry.isDirectory()) {
+			entryFile.mkdirs();
+		} else {
+			entryFile.getParentFile().mkdirs();
+			InputStream in = null;
+			OutputStream out = null;
+			try {
+				in = zipFile.getInputStream(entry);
+				out = new FileOutputStream(entryFile);
+				copy(in, out);
+			} finally {
+				if (in != null) {
+					try {
+						in.close();
+					} catch (Exception e) {
+						// ignore
+					}
+				}
+				if (out != null) {
+					try {
+						out.close();
+					} catch (Exception e) {
+						// ignore
+					}
+				}
+			}
+		}
+	}
 	
+	private static void copy(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[16 * 1024];
+		int len;
+		while ((len = in.read(buffer)) >= 0) {
+			out.write(buffer, 0, len);
+		}
+	}
 }
